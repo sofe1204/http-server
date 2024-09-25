f51b741476f3f3dff7ae52c8a30cc98d8db76b08
sofe1204 <sofeskin@hotmail.com>
2024-09-25 08:41:04 +0200

    server sockets and static page for testing

--- src/main/java/com/server/httpserver/HttpServer.java
+++ src/main/java/com/server/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.server.httpserver;
 import com.server.httpserver.config.Configuration;
 import com.server.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("Server starting....");
@@ -10,5 +16,36 @@ public class HttpServer {
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("port: "+ conf.getPort());
         System.out.println("webroot: "+ conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort()); // server socket just to accept the connection
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream(); // to read
+            OutputStream outputStream = socket.getOutputStream(); //to write
+
+            // writing
+            String html = "<html><head><title>Java http server</title></head><body><h1>This page was served by java http server</h1></body</html>";
+
+            final String CRLF = "\n\r"; //13, 10 ascii
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +// status line  : http version response_code response_message;
+                    "Content-Length: " + html.getBytes().length + CRLF + // header
+                            CRLF +
+                            html +
+                            CRLF + CRLF ;
+
+            outputStream.write(response.getBytes());
+
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
